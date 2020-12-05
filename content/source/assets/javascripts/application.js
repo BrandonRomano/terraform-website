26f92e414b713cda297dc599e5d4fbff3a60880d
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-12-04 21:05:44 -0800

    JS: Big refactor to do less work when turbolinking to new page
    We were creating a lot of event handlers on every page transition. Nearly all of
    them could be delegated to `document`, which lets us reuse the same handlers on
    every page in a given turbolinks session. Which, come to think of it,
    re-using global behavior instead of reinstantiating it every time you navigate
    was supposed to be the main benefit of turbolinks in the first place.

--- content/source/assets/javascripts/application.js
+++ content/source/assets/javascripts/application.js
@@ -44,6 +44,83 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     setUpUIFeatures(document.body);
 });
 
+// Variables and stuff
+var selQuickNav = '#inner-quicknav > ul.dropdown';
+var selDocsSidenavs = '#docs-sidebar ul.nav.docs-sidenav';
+var sidebarControlsHTML =
+    '<div id="sidebar-controls">' +
+        '<div id="sidebar-filter" style="display: none;">' +
+            '<span class="glyphicon glyphicon-search"></span>' +
+            '<label for="sidebar-filter-field" class="sr-only sr-only-focusable">Filter page titles in sidebar navigation</label>' +
+            '<input type="search" id="sidebar-filter-field" class="form-control" name="sidebar-filter-field" role="search" placeholder="Filter page titles" />' +
+            '<button id="filter-close" class="glyphicon glyphicon-remove-circle" title="Reset filter"><span class="sr-only sr-only-focusable">Reset sidebar filter</span></button>' +
+        '</div>' +
+        '<div id="sidebar-buttons">' +
+            '<button id="toggle-button">Expand all</button>' +
+            ' | ' +
+            '<button id="filter-button" title="Shortcut: type the / key">Filter</button>' +
+        '</div>' +
+    '</div>';
+var quickNavHTML =
+    '<div id="inner-quicknav">' +
+        '<span id="inner-quicknav-trigger">' +
+            'Jump to Section' +
+            '<svg width="9" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M8.811 1.067a.612.612 0 0 0 0-.884.655.655 0 0 0-.908 0L4.5 3.491 1.097.183a.655.655 0 0 0-.909 0 .615.615 0 0 0 0 .884l3.857 3.75a.655.655 0 0 0 .91 0l3.856-3.75z" fill-rule="evenodd"/></svg>' +
+        '</span>' +
+        '<ul class="dropdown"></ul>' +
+    '</div>';
+
+var sidebarController = {
+    reset: function() {
+        $('#sidebar-filter-field').trigger("blur").val("");
+        $(selDocsSidenavs).find("li").show();
+        this.expandDefaults();
+        $('#toggle-button').html("Expand all");
+        $('#sidebar-buttons').show();
+        $('#sidebar-filter').hide();
+    },
+    expandAll: function() {
+        this.enableReset();
+        $(selDocsSidenavs).find('li').addClass('active');
+    },
+    expandDefaults: function() {
+        var docsSideNavs = $(selDocsSidenavs);
+        // Reset everything to inactive. .has-subnav is added during setup.
+        docsSideNavs.find('.has-subnav').removeClass("active");
+        // Activate current page, locked-open navs, and all their ancestors. (These
+        // classes propagate to ancestors during setup.)
+        docsSideNavs.find('.current, .nav-visible').addClass('active');
+        // Activate auto-expand navs, but leave their ancestors alone:
+        docsSideNavs.find('.nav-auto-expand').parent('li').addClass('active');
+    },
+    enableReset: function() {
+        $('#toggle-button').html("Reset");
+    },
+    showFilter: function() {
+        this.enableReset();
+        $('#sidebar-buttons').hide();
+        $('#sidebar-filter').show();
+        $('#sidebar-filter-field').focus();
+    },
+    // Filter as you type. This alters three things:
+    // - "active" class on subnavs
+    // - direct show/hide of <li>s
+    // - state of #toggle-button
+    performFilter: function() {
+        var filterRegexp = new RegExp($('#sidebar-filter-field').val(), 'i');
+        var sidebarLinks = $(selDocsSidenavs).find('a');
+        var matchingLinks = sidebarLinks.filter(function(index) {
+            return $(this).text().match(filterRegexp);
+        });
+        sidebarLinks.parent('li').hide();
+        $(selDocsSidenavs).find('li').removeClass('active'); // cleans up partial as-you-type searches
+        // make matches and their parents visible and expanded:
+        matchingLinks.parents('li').show().addClass('active');
+        // make direct children visible (if your search caught a subnav directly):
+        matchingLinks.parent('li').find('li').show();
+    },
+};
+
 // Modify the page layout, and set up terraform.io UI features
 function setUpUIFeatures(bodyElement) {
     "use strict";
@@ -59,140 +136,31 @@ function setUpUIFeatures(bodyElement) {
     var sidebarHeader = $("#docs-sidebar", bodyElement).find("h1,h2,h3,h4,h5,h6").not("#otherdocs").first();
     sidebarHeaderGrid.append(sidebarHeader);
 
-    // Collapse most subnavs, but reveal any that contain the
-    // current page. The a.current-page class is added during build by
-    // layouts/inner.erb.
-    var docsSidebar = $("#docs-sidebar ul.nav.docs-sidenav", bodyElement);
-    var subNavs = docsSidebar.find("ul").addClass("nav-hidden").parent("li");
-        // we leave the nav-hidden class alone after this.
-    function resetActiveSubnavs() {
-        subNavs.removeClass("active");
-        // Activate current page, locked-open navs, and all their ancestors:
-        docsSidebar.find("li").has(".current-page, .nav-visible").addClass("active");
-        // Activate auto-expand navs, but leave their ancestors alone:
-        docsSidebar.find(".nav-auto-expand").parent("li").addClass("active");
-        // Colored links for current page and its ancestors
-        docsSidebar.find("li").has(".current-page").addClass("current");
-    }
-    resetActiveSubnavs();
+    var docsSideNavs = $(selDocsSidenavs, bodyElement);
+    docsSideNavs.find("ul").addClass("nav-hidden");
+        // we leave the nav-hidden class alone after this. We just use it so
+        // non-JS users aren't left with no way to view the nav.
+    // Colored links for current page and its ancestors. The a.current-page
+    // class is added during build by layouts/inner.erb.
+    docsSideNavs.find("li").has(".current-page").addClass("current");
+    // The optional .nav-visible class locks open a section and all of its ancestors.
+    docsSideNavs.find('li').has('.nav-visible').addClass('nav-visible');
 
-    // CSS class that adds toggle controls:
-    subNavs.addClass("has-subnav");
-    // Toggle subnav expansion when clicking an area that isn't claimed by the
-    // header or content (usually the :before pseudo-element)
-    subNavs.on("click", function(e) {
-        if (e.target == this) {
-            $(this).toggleClass("active");
-        }
-    });
-    // If the subnav header doesn't link to a different page, use it as a toggle.
-    docsSidebar.find("a[href^='#']").on("click", function(e) {
-        e.preventDefault();
-        $(this).parent("li").trigger("click");
-    });
+    // Add toggle controls:
+    docsSideNavs.find("li").has("ul").addClass("has-subnav");
+    // Activate current page, locked-open navs, and all their ancestors:
+    docsSideNavs.find('.current, .nav-visible').addClass('active');
+    // Activate auto-expand navs, but leave their ancestors alone:
+    docsSideNavs.find('.nav-auto-expand').parent('li').addClass('active');
 
     // If this is a Very Large Sidebar, add extra controls to expand/collapse
     // and filter it.
-    var sidebarLinks = docsSidebar.find("a");
+    var sidebarLinks = docsSideNavs.find("a");
     if (sidebarLinks.length > 30) {
         if ($("#sidebar-controls", bodyElement).length === 0) { // then add it!
-            var sidebarControlsHTML =
-                '<div id="sidebar-controls">' +
-                    '<div id="sidebar-filter">' +
-                        '<span class="glyphicon glyphicon-search"></span>' +
-                        '<label for="sidebar-filter-field" class="sr-only sr-only-focusable">Filter page titles in sidebar navigation</label>' +
-                        '<input type="search" id="sidebar-filter-field" class="form-control" name="sidebar-filter-field" role="search" placeholder="Filter page titles" />' +
-                        '<button id="filter-close" class="glyphicon glyphicon-remove-circle" title="Reset filter"><span class="sr-only sr-only-focusable">Reset sidebar filter</span></button>' +
-                    '</div>' +
-                    '<div id="sidebar-buttons">' +
-                        '<button id="toggle-button">Expand all</button>' +
-                        ' | ' +
-                        '<button id="filter-button" title="Shortcut: type the / key">Filter</button>' +
-                    '</div>' +
-                '</div>';
             sidebarHeaderGrid.append(sidebarControlsHTML);
         }
-
-        var filterDiv = $("div#sidebar-filter", bodyElement);
-        var buttonsDiv = $("div#sidebar-buttons", bodyElement);
-        var subnavToggle = $("#sidebar-controls #toggle-button", bodyElement);
-        var filterField = $("#sidebar-controls input#sidebar-filter-field", bodyElement);
-        var filterButton = $("#filter-button", bodyElement);
-
-        filterDiv.hide();
-
-        filterButton.on("click", function(e) {
-            buttonsDiv.hide();
-            filterDiv.show();
-            filterField.focus();
-        });
-
-        // Filter field's close button: defer to reset button.
-        $("#filter-close", bodyElement).on("click", function(e) {
-            subnavToggle.trigger("reset");
-        });
-
-        // Expand/reset button behavior:
-        subnavToggle.on({
-            "taint": function(e) {
-                $(this).html("Reset");
-            },
-            "reset": function(e) {
-                filterField.val("");
-                filterField.trigger("blur");
-                sidebarLinks.parent("li").show();
-                resetActiveSubnavs();
-                $(this).html("Expand all");
-                buttonsDiv.show();
-                filterDiv.hide();
-            },
-            "click": function(e) {
-                e.preventDefault();
-                if ($(this).text() === "Expand all") {
-                    subNavs.addClass("active");
-                    $(this).trigger("taint");
-                } else {
-                    $(this).trigger("reset");
-                }
-            }
-        });
-
-        // Filter as you type. This alters three things:
-        // - "active" class on subnavs
-        // - direct show/hide of <li>s
-        // - state of subnavToggle button
-        // We rely on subnavToggle's "reset" event to clean up when done.
-        filterField.on('keyup', function(e) {
-            if (e.keyCode === 27) { // escape key
-                subnavToggle.trigger("reset");
-            } else {
-                subnavToggle.trigger("taint");
-                var filterRegexp = new RegExp(filterField.val(), 'i');
-                var matchingLinks = sidebarLinks.filter(function(index) {
-                    return $(this).text().match(filterRegexp);
-                });
-                sidebarLinks.parent('li').hide();
-                subNavs.removeClass('active'); // cleans up partial as-you-type searches
-                // make matches and their parents visible and expanded:
-                matchingLinks.parents('li').show().filter(subNavs).addClass('active');
-                // make direct children visible (if your search caught a subnav directly):
-                matchingLinks.parent('li').find('li').show();
-            }
-        });
-        // Type slash to focus sidebar filter:
-        $(bodyElement).keydown(function(e) {
-            // 191 = / (forward slash) key
-            if (e.keyCode !== 191) {
-                return;
-            }
-            var focusedElementType = $(document.activeElement).get(0).tagName.toLowerCase();
-            if (focusedElementType !== "textarea" && focusedElementType !== "input") {
-                e.preventDefault();
-                filterButton.trigger("click");
-            }
-        });
     }
-
 
     // Move the main title into the grid container, so we can make things line up nicely.
     var innerHeaderGrid = $('#inner-header-grid', bodyElement);
@@ -203,39 +171,20 @@ function setUpUIFeatures(bodyElement) {
     var headers = $('#inner', bodyElement).find('h2, h3, h4');
     if (headers.length > 2 && $("div#inner-quicknav", bodyElement).length === 0) {
         // Build the quick-nav HTML:
-        innerHeaderGrid.append(
-            '<div id="inner-quicknav">' +
-                '<span id="inner-quicknav-trigger">' +
-                    'Jump to Section' +
-                    '<svg width="9" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M8.811 1.067a.612.612 0 0 0 0-.884.655.655 0 0 0-.908 0L4.5 3.491 1.097.183a.655.655 0 0 0-.909 0 .615.615 0 0 0 0 .884l3.857 3.75a.655.655 0 0 0 .91 0l3.856-3.75z" fill-rule="evenodd"/></svg>' +
-                '</span>' +
-                '<ul class="dropdown"></ul>' +
-            '</div>'
-        );
-        var quickNav = $('#inner-quicknav > ul.dropdown', bodyElement);
+        innerHeaderGrid.append(quickNavHTML);
+        var quickNav = $(selQuickNav, bodyElement);
         headers.each(function(index, element) {
-            var level = element.nodeName.toLowerCase();
-            var header_text = $(element).text();
-            var header_id = $(element).attr('id');
-            quickNav.append('<li class="level-' + level + '"><a href="#' + header_id + '">' + header_text + '</a></li>');
+            var a = document.createElement('a');
+            a.setAttribute('href', '#' + element.id);
+            a.textContent = element.innerText;
+
+            var li = document.createElement('li');
+            li.className = 'level-' + element.nodeName.toLowerCase();
+            li.append(a);
+
+            quickNav.append(li);
         });
-        // Attach event listeners:
-        // Trigger opens and closes.
-        $(bodyElement).on('click', function(e) {
-            var $target = $(e.target);
-            if ($target.is('#inner-quicknav #inner-quicknav-trigger')) {
-                // clicking trigger toggles quick-nav.
-                quickNav.toggleClass('active');
-            } else if ($target.is('#inner-quicknav > ul.dropdown li a')) {
-                // Jumping to a section means you're done with quick-nav.
-                quickNav.removeClass('active');
-            } else if ($target.closest(quickNav).length > 0) {
-                // Do nothing, clicking inside quick-nav doesn't close it.
-            } else {
-                // Clicking outside quick-nav closes it.
-                quickNav.removeClass('active');
-            }
-        });
+        // Event listener is on document, outside this setup.
     }
 
     // Downloads CTA
@@ -256,3 +205,67 @@ function setUpUIFeatures(bodyElement) {
     });
 
 }
+
+// Misc event handlers that don't need to change on every turbolinks transition,
+// and can thus just live forever.
+$(document).on('click', function(e) {
+    var target = $(e.target);
+    var quickNav = $(selQuickNav);
+
+    // SIDEBAR STUFF:
+    if ( target.is('.has-subnav') ) {
+        // toggle arrow toggles its subnav.
+        target.toggleClass('active');
+    } else if ( target.is(".has-subnav > a[href^='#']") ) {
+        // links that don't go anywhere act as toggle controls.
+        e.preventDefault();
+        target.parent().toggleClass('active');
+    } else if ( target.is('#filter-button') ) {
+        sidebarController.showFilter();
+    } else if ( target.is('#filter-close') ) {
+        sidebarController.reset();
+    } else if ( target.is('#toggle-button') ) {
+        if ( $('#toggle-button').text() === "Expand all" ) {
+            sidebarController.expandAll();
+        } else {
+            sidebarController.reset();
+        }
+    }
+
+    // QUICK-NAV STUFF: (not mutually exclusive w/ sidebar stuff, due to the final 'else')
+    if (target.closest('#inner-quicknav-trigger').length > 0) {
+        // clicking trigger (or its svg child) toggles quick-nav.
+        quickNav.toggleClass('active');
+    } else if (target.is('#inner-quicknav > ul.dropdown li a')) {
+        // Jumping to a section means you're done with quick-nav.
+        quickNav.removeClass('active');
+    } else if (target.closest(quickNav).length > 0) {
+        // clicking inside quick-nav doesn't close it.
+        // Do nothing.
+    } else {
+        // Clicking outside quick-nav closes it.
+        quickNav.removeClass('active');
+    }
+});
+
+$(document).on('keyup', function(e) {
+    if ( e.target === document.getElementById('sidebar-filter-field') ) {
+        if (e.keyCode === 27) { // escape key
+            sidebarController.reset();
+        } else {
+            sidebarController.performFilter();
+        }
+    }
+});
+
+$(document).on('keydown', function(e) {
+    // 191 = / (forward slash) key
+    if (e.keyCode !== 191) {
+        return;
+    }
+    var focusedElementType = document.activeElement.tagName;
+    if (focusedElementType !== "TEXTAREA" && focusedElementType !== "INPUT") {
+        e.preventDefault();
+        sidebarController.showFilter();
+    }
+});
